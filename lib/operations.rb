b27843ae43f6941e7e501d23ec6a1062009eb9df
Tyler Firth <tyfirth@gmail.com>
2020-05-06 04:40:21 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,20 @@
+require 'pry'
+
 def unsafe?(speed)
 
+if speed < 40 || speed > 60
+  return true
+else 
+  return false
+ end
+ 
 end
 
 
 
 def not_safe?(speed)
+	
+ speed > 60 || speed < 40 ? true : false
 	
 end
 	
